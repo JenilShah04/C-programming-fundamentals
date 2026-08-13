b19073d2186b6989f963ba644e36954870499304
JenilShah04 <jenilshah0412@gmail.com>
2026-08-13 23:30:05 +0530

    Add program 4 - Divide Two numbers

---
+++ 04_Divide_Two_Numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter Two Numbers\n");
+    scanf("%f %f",&a,&b);
+    c = a/b;
+    printf("Division of %f/%f=%f\n",a,b,c);
+    return 0;
+}
