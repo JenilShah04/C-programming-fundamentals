c8e5ef8c8fa01c83656cad0acee0d06abf118927
JenilShah04 <jenilshah0412@gmail.com>
2026-08-13 22:52:41 +0530

    Add Program 3 - Multiply Two Number

---
+++ 03_Multiply_Two_Numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter Two Numbers\n");
+    scanf("%d %d",&a,&b);
+    c = a*b;
+    printf("Multiplication of %d*%d=%d\n",a,b,c);
+    return 0;
+}
