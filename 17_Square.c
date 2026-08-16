384854d7e4cd46f883e2119e7b29070c91dbeef2
JenilShah04 <jenilshah0412@gmail.com>
2026-08-16 23:03:19 +0530

    Program 17 - Area and Perimeter of Square

---
+++ 17_Square.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int L,A,P;
+    printf("Enter the length of square:");
+    scanf("%d",&L);
+    A = L*L;
+    P = 4*L;
+    printf("Area of square:%d\n",A);
+    printf("Perimeter of square:%d",P);
+    return 0;
+
+}
