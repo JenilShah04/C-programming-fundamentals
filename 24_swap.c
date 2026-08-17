c6fa9bfc0aa02d03187b697fb5245fdfc2b29c05
JenilShah04 <jenilshah0412@gmail.com>
2026-08-17 22:12:08 +0530

    Add Program 24 - Swapping Two Numbers

---
+++ 24_swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+
+    // Swapping
+    c = a;
+    a = b;
+    b = c;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
