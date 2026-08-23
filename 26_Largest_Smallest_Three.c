95b75e0b507a671a2467b8bbde7f843514377616
JenilShah04 <jenilshah0412@gmail.com>
2026-08-23 22:27:02 +0530

    26 - Largest and Smallest of three values

---
+++ 26_Largest_Smallest_Three.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three numbers:");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>=b && a>=c)
+    {
+        printf("largest number is %d\n",a);
+    }
+    else if(b>=a && b>=c)
+    {
+         printf("largest number is %d\n",b);
+    }
+    else
+    {
+         printf("largest number is %d\n",c);
+    }
+     if(a<=b && a<=c)
+    {
+        printf("smallest number is %d\n",a);
+    }
+    else if(b<=a && b<=c)
+    {
+         printf("smallest number is %d",b);
+    }
+    else
+    {
+         printf("smallest number is %d\n",c);
+    }
+    return 0;
+
+}
